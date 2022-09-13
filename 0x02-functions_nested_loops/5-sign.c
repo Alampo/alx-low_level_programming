aa01deca48cd6408ba0199a11513c1f1d6556ba7
Alampo <alamindayo@gmail.com>
2022-09-13 15:04:38 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * print_sign - Print sign of numbers
+  *@n: input number as an integer
+  * Return: 1, if positive, -1 if negative, else 0
+  */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('43');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('45');
+		return (-1);
+	}
+	else
+	{
+		_putchar('48');
+		return (0);
+	}
+	_putchar('\n');
+}
