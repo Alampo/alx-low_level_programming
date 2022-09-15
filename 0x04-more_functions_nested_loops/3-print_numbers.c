e40302e9136242dd137663c7265b8645f0a93103
Alampo <alamindayo@gmail.com>
2022-09-15 21:41:53 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *print_numbers - Prints all numbers
+  *
+  *
+  *
+  *Return: No return
+  */
+void print_numbers(void)
+{
+	int c;
+
+	for (ch = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
