b0a56925a8f9a84f709495f7a11dbd8429e2ac69
Alampo <alamindayo@gmail.com>
2022-09-20 01:52:34 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *_strcpy - copies a string into another
+  *
+  *@src: string to be copied
+  *
+  *@dest: buffer to be copied to
+  *
+  *Return: pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++
+	}
+	return (dest);
+}
