710c117f576b366f8cc51b5320c41cc44b583438
Alampo <alamindayo@gmail.com>
2022-09-30 22:20:22 +0000

    Adding program file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - prints all arguments it recieves
+  *
+  *@argc: number of command line arguments
+  *@argv: an array of all command line arguments
+  *
+  *Return: 0 (success)
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i])
+	}
+	return (0);
+}
