6c6f5933ca82c7fcbdfd8bf3c380a71be37b70d4
Alampo <alamindayo@gmail.com>
2022-10-02 01:35:41 +0000

    adding libmy file

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *_strstr - finds a string in another string
+  *
+  *@haystack: 1st string
+  *@needle: second string
+  *
+  *Return: 0
+  */
+char *_strstr(char *haystack __attribute__((unused)), char *needle __attribute__((unused)))
+{
+	return (0);
+}
