fce664c4d89bf2bda0ec5836dbfe56975d2069f2
Alampo <alamindayo@gmail.com>
2022-10-12 23:20:00 +0000

    TAsk 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointer.h"
+
+/**
+  *int_index - ssearches for an inteeger
+  *@array: input integer array
+  *@size: size of the array
+  *@cmp: pointer to the function to be used
+  *
+  *Return: -1 and searched integer i
+  *
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
+	return (-1);
+}
