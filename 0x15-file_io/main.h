e892f33f562650a8d5e742696cf342d0a40c6623
Alampo <alamindayo@gmail.com>
2022-10-31 20:34:17 +0100

    Task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+#endif
